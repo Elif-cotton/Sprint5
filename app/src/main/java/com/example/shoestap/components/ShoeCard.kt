e5afc4498cd83afb3bb93e12d8bb053d72caa8d7
“elif-cotton” <elif.watercolors@gmail.com>
2024-09-23 20:36:07 -0300

    Modificar card

--- app/src/main/java/com/example/shoestap/components/ShoeCard.kt
+++ app/src/main/java/com/example/shoestap/components/ShoeCard.kt
@@ -6,20 +6,25 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.shoestap.model.Shoe
-
 @Composable
 fun ShoeCard(shoe: Shoe) {
     Card(
         modifier = Modifier
             .padding(8.dp)
-            .width(150.dp)
+            .width(180.dp)
     ) {
-        Column {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally, // Center the content
+            modifier = Modifier.padding(5.dp) // Padding around the entire column
+        ) {
             Image(
                 painter = rememberAsyncImagePainter(shoe.imageUrl),
                 contentDescription = shoe.name,
@@ -29,8 +34,18 @@ fun ShoeCard(shoe: Shoe) {
                 contentScale = ContentScale.Crop
             )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = shoe.name, style = MaterialTheme.typography.bodyLarge)
-            Text(text = shoe.formattedPrice, style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = shoe.name,
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold), // Make the text bold
+                modifier = Modifier.padding(5.dp), // Padding around the text
+                textAlign = TextAlign.Center // Center text horizontally
+            )
+            Text(
+                text = shoe.formattedPrice,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(5.dp), // Padding around the price text
+                textAlign = TextAlign.Center // Center text horizontally
+            )
         }
     }
 }
